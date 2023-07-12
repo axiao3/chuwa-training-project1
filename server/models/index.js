1b4177c0fb26747d905287ea35fa21809eca290c
Zixin Li <zli@quantumphotonics.club>
2023-07-11 22:16:05 -0400

    set mongodb_url

--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  // keepAlive: true,
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    // keepAlive: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected"));
 
 module.exports.User = require("./user");
 module.exports.Item = require("./item");
